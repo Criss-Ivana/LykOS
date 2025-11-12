d86a4314df952ac1d5236869e4daf79b45a52d10
Matei Lupu <matei.lupu1104@gmail.com>
2025-11-12 10:09:15 +0200

    Added spinlock to log.c

--- kernel/source/log.c
+++ kernel/source/log.c
@@ -2,6 +2,9 @@
 
 #include "arch/serial.h"
 #include "utils/printf.h"
+#include "sync/spinlock.h"
+
+static spinlock_t slock = SPINLOCK_INIT;
 
 void log(log_level_t level, const char *format, ...)
 {
@@ -10,9 +13,11 @@ void log(log_level_t level, const char *format, ...)
 
     char buf[256];
     vsnprintf(buf, 256, format, vargs);
-    serial_write(buf);
 
+    spinlock_acquire(&slock);
+    serial_write(buf);
     serial_write("\n");
+    spinlock_release(&slock);
 
     va_end(vargs);
 }
