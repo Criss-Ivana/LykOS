408ae0a2cd98ab75053e1a5bcea135856ea1cd6f
Matei Lupu <matei.lupu1104@gmail.com>
2026-01-01 18:12:49 +0200

    Small mmap stub fix.

--- kernel/source/sys/memory.c
+++ kernel/source/sys/memory.c
@@ -2,6 +2,7 @@
 
 #include "fs/vfs.h"
 #include "log.h"
+#include "mm/mm.h"
 #include "proc/proc.h"
 #include "proc/sched.h"
 #include "proc/thread.h"
@@ -13,8 +14,12 @@
 #define PROT_WRITE 0x02
 #define PROT_EXEC  0x04
 
-#define MAP_FAILED  ((void *)(-1))
-#define MAP_ANON    0x20
+#define MAP_FAILED   ((void *)(-1))
+#define MAP_FILE     0x00
+#define MAP_SHARED   0x01
+#define MAP_PRIVATE  0x02
+#define MAP_FIXED    0x10
+#define MAP_ANON     0x20
 
 sys_ret_t syscall_mmap(uintptr_t addr, size_t length, int prot, int flags, int fd, size_t offset)
 {
@@ -22,7 +27,7 @@ sys_ret_t syscall_mmap(uintptr_t addr, size_t length, int prot, int flags, int f
     vm_addrspace_t *as = proc->as;
 
     size_t value, err;
-    err = vm_map_vnode(as, addr, length, 0, VM_MAP_ANON | VM_MAP_PRIVATE, NULL, 0, &value);
+    err = vm_map_vnode(as, addr, length, MM_PROT_WRITE | MM_PROT_USER, VM_MAP_ANON | VM_MAP_PRIVATE, NULL, 0, &value);
 
     return (sys_ret_t) {
         value,
